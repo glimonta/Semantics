24626ac4616184875dc75b5d0a9028ce560d23f4
Gabriela Limonta <glimonta@gmail.com>
2015-03-01 01:31:14 +0100

    Exercise 12 was a written homework

---
+++ Exercise12/GabrielaLimonta.thy
@@ -0,0 +1,9 @@
+(** Score: 9/10 *)
+
+HW12.1:
+  Your construction A' is not always finite: what if A = {infinity}? then A' = N
+
+  For the prefix order, you should mention why B* has no upper bound, i.e.:
+
+    If there exists a top element, then top @ [True] would be greater than top. Contradiction.
+
